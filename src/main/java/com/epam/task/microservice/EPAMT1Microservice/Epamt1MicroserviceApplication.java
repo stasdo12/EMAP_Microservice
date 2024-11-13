7bf14f2649ab1c74913f2b5066b24fc49425189a
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-13 17:46:40 +0200

    add eureka dependency

--- src/main/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplication.java
+++ src/main/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplication.java
@@ -2,8 +2,12 @@ package com.epam.task.microservice.EPAMT1Microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Epamt1MicroserviceApplication {
 
 	public static void main(String[] args) {
