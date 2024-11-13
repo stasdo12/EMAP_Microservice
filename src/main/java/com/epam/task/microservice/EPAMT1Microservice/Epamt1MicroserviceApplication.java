31ace6c861d54f077b8398204f8ace4d82725768
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-13 15:57:06 +0200

    initial commit

---
+++ src/main/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.task.microservice.EPAMT1Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epamt1MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epamt1MicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.task.microservice.EPAMT1Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Epamt1MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
