b6e0f478065c5d71641d8e6d2daa440fa69f96ec
Stanislav Donetc <stanislavdonetc@gmail.com>
2024-11-15 14:24:20 +0200

    add feign client annotation

--- src/main/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplication.java
+++ src/main/java/com/epam/task/microservice/EPAMT1Microservice/Epamt1MicroserviceApplication.java
@@ -3,11 +3,12 @@ package com.epam.task.microservice.EPAMT1Microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Epamt1MicroserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/epam/task/microservice/EPAMT1Microservice/controller/TrainingWorkController.java
+++ src/main/java/com/epam/task/microservice/EPAMT1Microservice/controller/TrainingWorkController.java
@@ -6,8 +6,12 @@ import com.epam.task.microservice.EPAMT1Microservice.service.TrainingWorkService
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/training-work")
